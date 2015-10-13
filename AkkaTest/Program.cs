6aded99a0bdcf32c589fe2959dc22f9fdd21abe1
Christian Sparre <christian@sparre.it>
2015-10-13 12:23:49 +0200

    Akka.Remote test utility

--- AkkaTest/Program.cs
+++ AkkaTest/Program.cs
@@ -8,6 +8,7 @@ using System.Xml.Linq;
 using Akka.Actor;
 using Akka.Cluster;
 using Akka.Configuration;
+using Akka.Dispatch.SysMsg;
 
 namespace AkkaTest
 {
@@ -17,32 +18,26 @@ namespace AkkaTest
         {
             var config = @"
                 akka {
-                    stdout-loglevel = WARNING
-                    loglevel = WARNING
+                    stdout-loglevel = DEBUG
+                    loglevel = DEBUG
                     log-config-on-start = off
 
                     actor {
-                        provider = ""Akka.Cluster.ClusterActorRefProvider, Akka.Cluster""
+                        provider = ""Akka.Remote.RemoteActorRefProvider, Akka.Remote""
                     }
 
                     remote {
-                        log-remote-lifecycle-events = ERROR
+                        log-remote-lifecycle-events = DEBUG
                         helios.tcp {
                             hostname = ""127.0.0.1""
                             port = " + args[0] + @"
                         }
                     }
-
-                    cluster {
-                        seed-nodes = [" + string.Join(",", args.Skip(1).Select(x => $"\"akka.tcp://akkatest@127.0.0.1:{x}\""))  + @"],
-                        auto-down-unreachable-after = off
-                    }
                 }";
 
             var actorSystem = ActorSystem.Create("akkatest", ConfigurationFactory.ParseString(config));
-            var cluster = Cluster.Get(actorSystem);
             var pingActor = actorSystem.ActorOf<PingActor>("ping");
-            
+
 
             while (true)
             {
@@ -50,23 +45,20 @@ namespace AkkaTest
                 var commandArgs = command.Split(' ');
                 switch (commandArgs[0].ToLowerInvariant())
                 {
-                    case "join":
-                        Join(commandArgs, cluster);
-                        break;
-                    case "join-seed":
-                        JoinSeedNodes(commandArgs, cluster);
-                        break;
-                    case "leave":
-                        Leave(commandArgs, cluster);
-                        break;
-                    case "down":
-                        Down(commandArgs, cluster);
-                        break;
                     case "status":
-                        Status(cluster);
+                        Status(actorSystem);
                         break;
                     case "ping":
                         Ping(actorSystem, commandArgs);
+                        break;
+                    case "watch":
+                        Watch(actorSystem, commandArgs);
+                        break;
+                    case "reliable-watch":
+                        ReliableWatch(actorSystem, commandArgs);
+                        break;
+                    case "unwatch":
+                        Unwatch(actorSystem, commandArgs);
                         break;
                     default:
                         Console.WriteLine($"Don't know command \"{command}\"");
@@ -76,51 +68,55 @@ namespace AkkaTest
             }
         }
 
-        private static void Join(string[] commandArgs, Cluster cluster)
+        private static void ReliableWatch(ActorSystem actorSystem, string[] commandArgs)
         {
-            var address1 = Address.Parse($"akka.tcp://akkatest@127.0.0.1:{commandArgs[1]}");
-            Console.WriteLine($"Joining {address1}");
-            cluster.Join(address1);
-        }
-
-        private static void JoinSeedNodes(string[] commandArgs, Cluster cluster)
-        {
-            var builder = ImmutableList<Address>.Empty.ToBuilder();
-            foreach (var port in commandArgs.Skip(1))
+            var port = 0;
+            if (!int.TryParse(commandArgs[1], out port))
             {
-                builder.Add(Address.Parse($"akka.tcp://akkatest@127.0.0.1:{port}"));
+                Console.WriteLine("Invalid port: " + commandArgs[1]);
+                return;
             }
-            var list = builder.ToImmutable();
-            Console.WriteLine("Joining Seed Nodes:\n" + string.Join("\n   ", list.Select(x => $"{x}")));
-            cluster.JoinSeedNodes(list);
+            actorSystem.ActorOf(Props.Create(() => new ReliableWatcher(port)), "watcher" + port);
         }
 
-        private static void Leave(string[] commandArgs, Cluster cluster)
+        private static void Unwatch(ActorSystem actorSystem, string[] commandArgs)
         {
-            var address3 = Address.Parse($"akka.tcp://akkatest@127.0.0.1:{commandArgs[1]}");
-            Console.WriteLine($"Leaving {address3}");
-            cluster.Leave(address3);
+            var resolveOne = actorSystem.ActorSelection("user/watcher" + commandArgs[1]).ResolveOne(TimeSpan.FromSeconds(1));
+
+            resolveOne.ContinueWith(task =>
+            {
+                if (task.IsFaulted)
+                {
+                    Console.WriteLine("Could not find watcher" + commandArgs[1]);
+                    return;
+                }
+                resolveOne.Result.Tell(PoisonPill.Instance);
+            });
         }
 
-        private static void Down(string[] commandArgs, Cluster cluster)
+        private static void Watch(ActorSystem actorSystem, string[] commandArgs)
         {
-            var address2 = Address.Parse($"akka.tcp://akkatest@127.0.0.1:{commandArgs[1]}");
-            Console.WriteLine($"Downing {address2}");
-            cluster.Down(address2);
+            var port = 0;
+            if (!int.TryParse(commandArgs[1], out port))
+            {
+                Console.WriteLine("Invalid port: " + commandArgs[1]);
+                return;
+            }
+            actorSystem.ActorOf(Props.Create(() => new Watcher(port)), "watcher" + port);
         }
 
-        private static void Status(Cluster cluster)
+        private static void Status(ActorSystem actorSystem)
         {
-            Console.WriteLine("Self: " + cluster.SelfUniqueAddress);
-            Console.WriteLine("ClusterStatus: " + GetStatus(cluster));
-            Console.WriteLine("ClusterLeader: " + cluster.ReadView.Leader);
-            Console.WriteLine("Members:");
-            Console.WriteLine("   " +
-                              string.Join("\n   ", cluster.ReadView.Members.Select(x => $"{x.UniqueAddress} - {x.Status}")));
-            Console.WriteLine("UnreachableMembers: ");
-            Console.WriteLine("   " +
-                              string.Join("\n   ",
-                                  cluster.ReadView.Reachability.AllUnreachable.Select(x => $"{x.Address} {x.Uid}")));
+            //Console.WriteLine("Self: " + cluster.SelfUniqueAddress);
+            //Console.WriteLine("ClusterStatus: " + GetStatus(cluster));
+            //Console.WriteLine("ClusterLeader: " + cluster.ReadView.Leader);
+            //Console.WriteLine("Members:");
+            //Console.WriteLine("   " +
+            //                  string.Join("\n   ", cluster.ReadView.Members.Select(x => $"{x.UniqueAddress} - {x.Status}")));
+            //Console.WriteLine("UnreachableMembers: ");
+            //Console.WriteLine("   " +
+            //                  string.Join("\n   ",
+            //                      cluster.ReadView.Reachability.AllUnreachable.Select(x => $"{x.Address} {x.Uid}")));
         }
 
         private static void Ping(ActorSystem actorSystem, string[] commandArgs)
@@ -139,19 +135,81 @@ namespace AkkaTest
                     }
                 });
         }
+    }
 
-        private static string GetStatus(Cluster cluster)
+    public class Watcher : ReceiveActor
+    {
+        private readonly int _port;
+
+        public Watcher(int port)
         {
+            _port = port;
+            //Context.System.EventStream.Subscribe(Self, typeof(Terminated));
+
+            var actorRef = Context.ActorSelection($"akka.tcp://akkatest@127.0.0.1:{port}/user/ping").ResolveOne(TimeSpan.FromSeconds(1)).Result;
+            Context.Watch(actorRef);
+
+           //Receive<Terminated>(m => HandleTerminated(m));
+        }
+
+        private void HandleTerminated(Terminated message)
+        {
+            Console.WriteLine($"{message.ActorRef.Path.Address} terminated");
+            Context.Stop(Self);
+        }
+
+        protected override void PostStop()
+        {
+            Console.WriteLine($"watcher{_port} stopped");
+
+        }
+    }
+
+    public class ReliableWatcher : ReceiveActor
+    {
+        private readonly int _port;
+
+        public ReliableWatcher(int port)
+        {
+            _port = port;
+
+            var actorRef = Context.ActorSelection($"akka.tcp://akkatest@127.0.0.1:{port}/user/ping").ResolveOne(TimeSpan.FromSeconds(1)).Result;
+            Context.Watch(actorRef);
+
+            Receive<Terminated>(m => HandleTerminated(m));
+            Receive<string>(x => x == "Ping", m => TryPing());
+        }
+
+        private async void TryPing()
+        {
+            var actorRefTask = Context.ActorSelection($"akka.tcp://akkatest@127.0.0.1:{_port}/user/ping").ResolveOne(TimeSpan.FromSeconds(1));
+
             try
             {
-                return cluster.ReadView.Status.ToString();
+                var actorRef = actorRefTask.Result;
+                var pong = actorRef.Ask<string>("ping").Result;
+                Console.Write("Starting watcher because of: " + pong);
+                Context.Watch(actorRef);
             }
             catch (Exception)
             {
-                return "Got Exception";
-                throw;
+                Console.WriteLine("Failed pinging " + _port);
+                Context.System.Scheduler.ScheduleTellOnceCancelable(TimeSpan.FromSeconds(1), Self, "Ping", Self);
             }
             
+        }
+
+        private void HandleTerminated(Terminated message)
+        {
+            Console.WriteLine($"{message.ActorRef.Path.Address} terminated");
+
+            Context.System.Scheduler.ScheduleTellOnceCancelable(TimeSpan.FromSeconds(1), Self, "Ping", Self);
+        }
+
+        protected override void PostStop()
+        {
+            Console.WriteLine($"watcher{_port} stopped");
+
         }
     }
 
@@ -159,11 +217,9 @@ namespace AkkaTest
     {
         public PingActor()
         {
-            var cluster = Cluster.Get(Context.System);
-
             Receive<string>(x => x == "ping", s =>
             {
-                Sender.Tell("pong from " + cluster.SelfUniqueAddress.ToString());
+                Sender.Tell("pong from " + Self.Path.Address.ToString());
             });
         }
     }
